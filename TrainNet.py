a0af481776258fb2bd204539f551519e7ef61381
bpfinnerty <bpfinnerty24@gmail.com>
2022-11-24 20:46:29 -0500

    trying to get it to run

--- TrainNet.py
+++ TrainNet.py
@@ -11,31 +11,41 @@ def oneHotEncode(vector,numClasses):
     return encoded
 
 def main(learningRate, numThreads, epoch, batch_size, trainDataPath,testDataPath, model):
+    
     train_df = pd.read_csv(trainDataPath,header=None,skiprows=1)
+    print("opened data")
+    
     projNet.setLearningRate(learningRate)
     projNet.setThreads(numThreads)
-    
+    print("Set initial values")
     
     for e in range(0,epoch):
+        print("epoch: " + str(e))
+        
         shuffled = train_df.sample(frac=.1)
         truth = shuffled.iloc[:,0].to_numpy()
         train = shuffled.iloc[:,1:].to_numpy()
         encoded_truth = oneHotEncode(truth)
-        max_examples = train.truth
+        
+        
+        max_examples = train.size
         index = 0
         avgLoss = 0
+        print("ready for loop")
         while index < max_examples:
-            
+            print(index)
             projNet.zeroGrad()
             counter = 0
             while counter < batch_size:
                 if counter >= max_examples:
                     break
-                
+                print(counter)
                 predictions = model.forward(train[index])
                 loss = projNet.crossEntropy(predictions, encoded_truth[index])
                 avgLoss += loss
                 projNet.backwardStep(predictions,encoded_truth[index])
+                index+=1
+                counter+=1
             print("finished batch")
             projNet.updateWeights()
         print("Avg Loss: " + str(avgLoss/max_examples) + "for epoch: " + str(e))    
