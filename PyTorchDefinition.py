91764ae991e27fcc38e21fadad8f63feb45f1606
bpfinnerty <bpfinnerty24@gmail.com>
2022-11-26 13:52:11 -0500

    Good enough pytorchClassification

--- PyTorchDefinition.py
+++ PyTorchDefinition.py
@@ -7,22 +7,27 @@ class MLP(nn.Module):
 
     def __init__(self):
         super(MLP, self).__init__()
-        self.inputLayer = nn.linear(784,256)
-        self.h1 = nn.linear(256,128)
-        self.h2 = nn.linear(126,64)
-        self.h3 = nn.linear(64,32)
-        self.outputLayer = nn.linear(32,10)
+        self.inputLayer = nn.Linear(784,128)
+        # self.h1 = nn.Linear(256,128)
+        # self.h2 = nn.Linear(128,64)
+        self.h3 = nn.Linear(128,64)
+        self.outputLayer = nn.Linear(64,10)
 
     def forward(self, val):
         # implement your code here:
         # first layer of convolution with a max pooling
-        val = f.sigmoid(self.inputLayer(val))
+        val = self.inputLayer(val)
+        val = torch.sigmoid(val)
 
         # second layer of convolution with a max pooling
-        val = f.sigmoid(self.h1(val))
+        # val = self.h1(val)
+        # val = torch.sigmoid(val)
 
         # third layer of convolution with a max pooling
-        val = f.sigmoid(self.h2(val))
-        val = f.sigmoid(self.h3(val))
-        val = f.softmax(self.outputLayer(val))
+        # val = self.h2(val)
+        # val = torch.sigmoid(val)
+        val = self.h3(val)
+        val = torch.sigmoid(val)
+        val = self.outputLayer(val)
+        val = f.softmax(val,dim=1)
         return val
--- PyTorchExample.py
+++ PyTorchExample.py
@@ -5,38 +5,41 @@ import torch
 import torch.nn.functional as f
 import torch.utils.data as data_utils
 from PyTorchDefinition import *
+import os
 
 def main(learningRate, numThreads, epoch, batch_size, trainDataPath,testDataPath, model):
     
     train_df = pd.read_csv(trainDataPath,header=None,skiprows=1)
     test_df = pd.read_csv(testDataPath,header=None,skiprows=1)
     
-    
     optimizer = torch.optim.SGD(model.parameters(), lr=learningRate)
     criterion = nn.CrossEntropyLoss()
     
-    target = torch.tensor(train_df.iloc[:,0].values)
-    features = torch.tensor(train_df.iloc[:,1:].values)
-
+    trainLabelNumpy = train_df.iloc[:,0].to_numpy()
+    trainDataNumpy = train_df.iloc[:,1:].to_numpy()/255
+    
+    target = torch.tensor(trainLabelNumpy)
+    features = torch.tensor(trainDataNumpy)
+    torch.set_num_threads(8)
     train = data_utils.TensorDataset(features, target)
-    train_loader = data_utils.DataLoader(train, batch_size=batch_size, shuffle=True)
+    train_loader = data_utils.DataLoader(train, batch_size=1, shuffle=True)
     
-    target_test = torch.tensor(test_df.iloc[:,0].values)
-    test_features = torch.tensor(test_df.iloc[:,1:].values)
+    testLabelNumpy = test_df.iloc[:,0].to_numpy()
+    testDataNumpy = test_df.iloc[:,1:].to_numpy()/255
     
-    test = data_utils.TensorDataset(test_features, target_test)
+    target_test = torch.tensor(testLabelNumpy)
+    test_features = torch.tensor(testDataNumpy)
+    
+    test = data_utils.TensorDataset(test_features, target_test,)
     test_loader = data_utils.DataLoader(test, batch_size=1, shuffle=True)
-    
-    
     
     for e in range(epoch):
         model.train(True)
-        running_loss = 0.0
-        last_loss = 0.0
-        
+        running_loss = 0.0        
         for i, data in enumerate(train_loader):
             # Every data instance is an input + label pair
             inputs, labels = data
+            inputs = inputs.to(torch.float32)
 
             # Zero your gradients for every batch!
             optimizer.zero_grad()
@@ -53,42 +56,31 @@ def main(learningRate, numThreads, epoch, batch_size, trainDataPath,testDataPath
 
             # Gather data and report
             running_loss += loss.item()
-            if i % batch_size ==batch_size-1:
-                last_loss = running_loss / batch_size # loss per batch
-                print('  batch {} loss: {}'.format(i + 1, last_loss))
-                tb_x = e * len(train_loader) + i + 1
-                print("Loss/Train: " + str(last_loss) + ",  "+ str(tb_x))
-                running_loss = 0.0
+        
         
         model.train(False)
         accuracy = 0
-        counter = 0
+        
         for i, tdata in enumerate(test_loader):
             inputs,labels = tdata
+            inputs = inputs.to(torch.float32)
             output = model(inputs)
-            pred = torch.max(output)
+            pred = torch.argmax(output)
             if pred == labels:
                 accuracy +=1
-            counter += 1
-        print("Accuracy for epoch  " + str(e) + " = " +  str(accuracy/counter))
+                
+        print("Epoch: {0} Avg Loss: {1} Accuracy: {2}".format(e,running_loss/len(train_loader),accuracy/len(test_loader)))
             
-            
-
-        
-        
-        
-    
-    
 
 if __name__ == "__main__":
     
-    learningRate = 0.02
-    numThreads = 12
-    epoch = 3
+    learningRate = 0.04
+    numThreads = 8
+    epoch = 10
     batch = 100
     
-    trainingDataPath = "mnist_train.csv"
-    testingDataPath = "mnist_test.csv"
+    trainingDataPath = "DeepLearningLibrary/mnist_train.csv"
+    testingDataPath = "DeepLearningLibrary/mnist_test.csv"
     
     model = MLP()
     
